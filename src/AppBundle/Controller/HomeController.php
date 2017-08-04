1b30cbae9d7f488db5118e999b4f3958bb4bee0f
Joris Rietveld <jorisrietveld@gmail.com>
2017-08-04 13:11:28 +0200

    Moving from xlf to yml translations

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -4,17 +4,22 @@
  * Created: 16-07-2017 12:55
  * Licence: GPLv3 - General Pulbic Licence version 3
  */
+
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Contact;
+use AppBundle\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HomeController extends Controller
 {
-    public function homeAction()
-    {
-        return $this->render('AppBundle:Home:home.html.twig', array(
-            // ...
-        ));
-    }
+	public function homeAction()
+	{
+		$form = $this->createForm(ContactType::class );
+
+		return $this->render( 'AppBundle:Home:home.html.twig', [
+			'contactForm' => $form->createView()
+		] );
+	}
 
 }
